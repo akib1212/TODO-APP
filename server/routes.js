f1c8b63d30e21cec6171f5088cc27c0c629fe049
Akib Mahamud <akibmahamud55@gmail.com>
2024-11-03 20:58:42 +0600

    CURD functionality complete

--- server/routes.js
+++ server/routes.js
@@ -1,19 +1,19 @@
 import express from "express";
 const router = express.Router();
-import {getConnectedClient} from "./database.js";
-import { Collection } from "mongodb";
+import { getConnectedClient } from "./database.js";
+import { ObjectId } from "mongodb";
 
 
-const getCollection=()=>{
-    const client=getConnectedClient();
-    const collection=client.db("todosdb").collection("todos");
+const getCollection = () => {
+    const client = getConnectedClient();
+    const collection = client.db("todosdb").collection("todos");
     return collection;
 }
 
 // GET /todos (Get all the todos)
-router.get("/todos",async (req, res) => {
-    const collection=getCollection();
-    const todos=await collection.find({}).toArray();
+router.get("/todos", async (req, res) => {
+    const collection = getCollection();
+    const todos = await collection.find({}).toArray();
 
     res.status(200)
         .json(todos)
@@ -21,31 +21,35 @@ router.get("/todos",async (req, res) => {
 
 // POST /todos (Create todos)
 router.post("/todos", async (req, res) => {
-    const collection=getCollection();
-    const {todo}=req.body;
-    const newTodo=await collection.insertOne({todo,status:false});
+    const collection = getCollection();
+    const { todo } = req.body;
+    const newTodo = await collection.insertOne({ todo, status: false });
     console.log(todo);
-    
+
     res.status(201)
         .json({
-            todo,status:false,_id:newTodo.insertedId
+            todo, status: false, _id: newTodo.insertedId
         });
 })
 
 // DELETE /todos/:id (Delete todos)
-router.delete("/todos/:id", (req, res) => {
+router.delete("/todos/:id", async (req, res) => {
+    const collection = getCollection();
+    const _id = new ObjectId(req.params.id);
+    const deletedTodo = await collection.deleteOne({ _id });
     res.status(200)
-        .json({
-            message: "Delete request to /api/todos/:id"
-        })
+        .json(deletedTodo)
 })
 
 // PUT /todos/:id (Edit existing todos)
-router.put("/todos/:id", (req, res) => {
+router.put("/todos/:id", async (req, res) => {
+    const collection = getCollection();
+    const _id = new ObjectId(req.params.id);
+    const { status } = req.body;
+    const updatedTodo = await collection.updateOne({ _id }, { $set: { status: !status } })
+
     res.status(200)
-        .json({
-            message: "Put request to /api/todos/:id"
-        })
+        .json(updatedTodo);
 })
 
 
